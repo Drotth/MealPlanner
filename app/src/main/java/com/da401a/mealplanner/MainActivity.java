fc5da3326f399ca5d429b44d5a08fe297523b5b0
Andreas Drotth <drotth@hotmail.com>
2015-01-12 22:18:58 +0100

    Messing around with a week-day expandable listview and also a very custom flipanimation logo intro

--- app/src/main/java/com/da401a/mealplanner/MainActivity.java
+++ app/src/main/java/com/da401a/mealplanner/MainActivity.java
@@ -1,36 +1,54 @@
 package com.da401a.mealplanner;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewConfiguration;
 
+import java.lang.reflect.Field;
 
 public class MainActivity extends Activity {
+
+    FragmentManager fm;
+    WeekFragment weeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fm = getFragmentManager();
+        weeks = new WeekFragment();
+
+        // Force the Overflow Menu to show even on phones with dedicated menu button
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if(menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception exc) {}
+
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.container, weeks);
+        ft.commit();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_about) {
             return true;
         }
 
