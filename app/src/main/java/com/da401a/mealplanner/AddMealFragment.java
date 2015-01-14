2f26cc3e4981ee367a983fc51de50c4029258cff
Nilssonnet <nilssonnet@gmail.com>
2015-01-14 18:26:55 +0100

    Spinner still not ok :(

--- app/src/main/java/com/da401a/mealplanner/AddMealFragment.java
+++ app/src/main/java/com/da401a/mealplanner/AddMealFragment.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.database.CursorIndexOutOfBoundsException;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,15 +20,17 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AddMealFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+public class AddMealFragment extends Fragment{
     private DBController dbController;
     private EditText date, recipe;
-    private ArrayList<String> recipesList;
+    private List<String> recipesList;
+    private String[] recipeArray;
     private int userSelectedIndex;
 
     public AddMealFragment() {
@@ -44,10 +47,14 @@ public class AddMealFragment extends Fragment implements AdapterView.OnItemSelec
         recipe = (EditText) view.findViewById(R.id.editTextAddMealSelectMeal);
 
         Button buttonDone = (Button) view.findViewById(R.id.buttonAddMealDone);
+        Button newRecipe = (Button) view.findViewById(R.id.buttonAddMealNew);
+        EditText editDateSelect = (EditText) view.findViewById(R.id.editTextAddMealSelectDate);
+        final Spinner spinner = (Spinner) view.findViewById(R.id.spinnerShowRecipes);
+
         buttonDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newDate = date.getText().toString();
+                /*String newDate = date.getText().toString();
                 String newRecipe = recipe.getText().toString();
                 if(newDate.isEmpty() || newRecipe.isEmpty()){
                     Toast.makeText(getActivity(), "You have to fill in all fields",
@@ -56,12 +63,11 @@ public class AddMealFragment extends Fragment implements AdapterView.OnItemSelec
                     dbController.dataIntoWeekMeal(newDate);
                     date.setText("");
                     recipe.setText("");
-                }
+                }*/
 
             }
         });
 
-        Button newRecipe = (Button) view.findViewById(R.id.buttonAddMealNew);
         newRecipe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,59 +81,30 @@ public class AddMealFragment extends Fragment implements AdapterView.OnItemSelec
             }
         });
 
-        EditText editDateSelect = (EditText) view.findViewById(R.id.editTextAddMealSelectDate);
         editDateSelect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DialogFragment newFragment = new DatePickerFragment();
                 newFragment.show(getFragmentManager(), "datePicker");
             }
-
-            /*
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    showDialog(DIALOG_DATE_PICKER);
-                }
-                return false;
-            }
-            */
         });
 
-        Spinner spinner = (Spinner) view.findViewById(R.id.spinnerShowRecipes);
         ArrayAdapter<String> adp1=new ArrayAdapter<String> (getActivity(),
-                android.R.layout.simple_list_item_1,recipesList);
+                android.R.layout.simple_list_item_1, recipeArray);
         adp1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adp1);
-        spinner.setOnItemSelectedListener(this);
-
-        /*spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                Toast.makeText(getActivity(), "knas knas",
-                        Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
-            }
-
-        });*/
-        /*
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                userSelectedIndex = position;
-                Toast.makeText(getActivity(), "" + userSelectedIndex,
+                String result = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getActivity(), result,
                         Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+
             }
-        });*/
+        });
 
         return view;
     }
@@ -136,6 +113,9 @@ public class AddMealFragment extends Fragment implements AdapterView.OnItemSelec
     public void onResume() {
         super.onResume();
         dbController.open();
+
+        recipesList = new ArrayList<String>();
+
         Cursor c = dbController.getRecipeName();
         if(c != null && c.moveToFirst());
         do{
@@ -143,12 +123,13 @@ public class AddMealFragment extends Fragment implements AdapterView.OnItemSelec
                 recipesList.add(c.getString(0));
             } catch (CursorIndexOutOfBoundsException exception){}
         }while (c.moveToNext());
+
+        recipeArray = new String[recipesList.size()];
+        for (int i = 0; i < recipesList.size(); i++){
+            recipeArray[i] = recipesList.get(i);
+        }
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_spinner_item, recipesList);
-        //recipesList = dbController.getRecipeName();
-        /*Cursor c = dbController.getRecipes();
-        recipesAdapter = new RecipesAdapter(getActivity(), c, true);
-        listRecipes.setAdapter(recipesAdapter);*/
+                android.R.layout.simple_spinner_item, recipeArray);
     }
 
     @Override
@@ -161,23 +142,11 @@ public class AddMealFragment extends Fragment implements AdapterView.OnItemSelec
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         dbController = new DBController(getActivity());
-        recipesList = new ArrayList<String>();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         dbController.close();
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getActivity(), "knas knas",
-                Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
     }
 }
